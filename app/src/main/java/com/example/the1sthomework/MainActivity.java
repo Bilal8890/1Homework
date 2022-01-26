6724a8c85742e4adf329e1ecbe4d56c5545b62e9
Bilal8890 <94915210+Bilal8890@users.noreply.github.com>
2022-01-26 18:57:26 +0600

    first

---
+++ app/src/main/java/com/example/the1sthomework/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.the1sthomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
